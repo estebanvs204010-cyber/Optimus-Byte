75a6a883e7d008482da99118462c13c6324a2774
Joseph Ricardo Fonseca Salamanaca <josephfonseca2005@gmail.com>
2026-05-06 17:28:42 -0500

    vistas servicios

--- VistaPrincipal/Controllers/ServiciosController.cs
+++ VistaPrincipal/Controllers/ServiciosController.cs
@@ -1,10 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VistaPrincipal.Models;
 
-namespace VistaPrincipal.Controllers
+namespace TuProyecto.Controllers
 {
     public class ServiciosController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Diagnostico()
+        {
+            return View();
+        }
+
+        public IActionResult Mantenimiento()
+        {
+            return View();
+        }
+
+        public IActionResult Reparaciones()
+        {
+            return View();
+        }
+
+        public IActionResult Electricidad()
+        {
+            return View();
+        }
+
+        public IActionResult Inspecciones()
         {
             return View();
         }
