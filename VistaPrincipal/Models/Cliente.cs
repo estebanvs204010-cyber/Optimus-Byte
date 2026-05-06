7dab57578dc3dc4251776420e02a0aeed5d402ce
Andres <andresffonseca5@gmail.com>
2026-05-06 15:29:32 -0500

    agregue el login

--- VistaPrincipal/Models/Cliente.cs
+++ VistaPrincipal/Models/Cliente.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using VistaPrincipal.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace VistaPrincipal.Models
+[Table("Usuarios")]
+public class Usuario
 {
-    public class Cliente : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+    [Key]
+    [Column("id_usuario")]
+    public int IdUsuario { get; set; }
+
+    [Column("nombre_completo")]
+    public string NombreCompleto { get; set; } = string.Empty;
+
+    [Column("correo")]
+    public string Correo { get; set; } = string.Empty;
+
+    [Column("contrasena_hash")]
+    public string ContrasenaHash { get; set; } = string.Empty;
+
+    [Column("activo")]
+    public bool Activo { get; set; }
+
+    [Column("id_rol")]
+    public int IdRol { get; set; }
+
+    [ForeignKey("IdRol")] // 🔥 ESTA LÍNEA ES LA SOLUCIÓN
+    public Rol? Rol { get; set; }
+
+    [Column("telefono")]
+    public string Telefono { get; set; } = string.Empty;
+
+    [NotMapped]
+    [Required]
+    public string Contrasena { get; set; }
 }
